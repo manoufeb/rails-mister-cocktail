1c8cdb64acdb94e6cc20c1121fbd9443a75fcda9
Manou <mfebvret@gmail.com>
2016-08-11 15:09:08 +0200

    add controllers and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 Ingredient.destroy_all
+Cocktail.destroy_all
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
 Ingredient.create(name: "cherry")
 Ingredient.create(name: "vodka")
 Ingredient.create(name: "orange")
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "Sex on the beach")
+Cocktail.create(name: "Malibu night")
